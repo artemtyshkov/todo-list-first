efef6565fe98bd64af84c4f79a5124b4fc1fb8fa
Artem Tyshkov <artemtyshkov@gmail.com>
2023-07-26 16:54:34 +0300

    Added opacity for unordered list. Saving before new features

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const btn = document.querySelector('button');
 const popup = document.querySelector('.popup');
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
 
 btn.addEventListener('click', () => {
     popup.classList.toggle('show');
+    ul.style.opacity = '0.3';
+
+    if (!popup.classList.contains('show')) {
+        ul.style.opacity = '';
+    }
 });
+
