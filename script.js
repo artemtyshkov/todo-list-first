351c5ad74f3dda73306b356e0864f03706a77482
Artem Tyshkov <artemtyshkov@gmail.com>
2023-07-28 11:37:14 +0300

    Made buttons

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const btn = document.querySelector('button');
+const popupButton = document.querySelector('.popup-button');
+const cancelButton = document.querySelector('.cancel-button');
+const addButton = document.querySelector('.add-button');
 const popup = document.querySelector('.popup');
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
 
-btn.addEventListener('click', () => {
+popupButton.addEventListener('click', () => {
     popup.classList.toggle('show');
-    ul.style.opacity = '0.3';
+    ul.style.opacity = '0.5';
+    popupButton.classList.add('hide');
+    addButton.classList.add('show');
+    cancelButton.classList.add('show');
+
+
 
     if (!popup.classList.contains('show')) {
         ul.style.opacity = '';
