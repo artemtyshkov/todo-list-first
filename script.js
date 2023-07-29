ac8892d0e1b442ae38f8e0b14633ab027eb7feb8
Artem Tyshkov <artemtyshkov@gmail.com>
2023-07-29 10:29:00 +0300

    JS for buttons is done

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const input = document.querySelector('input');
 
 popupButton.addEventListener('click', () => {
     popup.classList.add('show');
+    input.focus();
     ul.style.opacity = '0.5';
     popupButton.classList.add('hide');
     addButton.classList.remove('hide');
@@ -17,11 +18,51 @@ popupButton.addEventListener('click', () => {
     }
 });
 
+addButton.addEventListener('click', () => {
+    if (input.value.trim() == '') {
+        firstStage();
+    } else {
+        addTask();
+    }
+});
+
+input.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') {
+        addTask();
+    }
+});
+
 cancelButton.addEventListener('click', () => {
+    firstStage();
+    input.value = '';
+});
+
+// functions
+
+function firstStage() {
     popup.classList.remove('show');
     ul.style.opacity = '';
     popupButton.classList.remove('hide');
     addButton.classList.add('hide');
     cancelButton.classList.add('hide');
-});
+}
 
+function addTask() {
+    let li = document.createElement('li');
+
+    li.innerHTML += `<div class="square"></div>${input.value}`;
+
+    ul.append(li);
+    saveTask();
+    input.value = '';
+    firstStage();
+}
+
+function saveTask() {
+    localStorage.setItem('task', `${input.value}`);
+}
+
+function getTasks() {
+    localStorage.getItem('task');
+}
+getTasks();
