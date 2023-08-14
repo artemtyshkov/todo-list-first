5882995e202268c6ce37a75f1e94f08f0f21558a
Artem Tyshkov <artemtyshkov@gmail.com>
2023-08-14 13:47:12 +0300

    render todos

--- script.js
+++ script.js
@@ -2,33 +2,39 @@ const popupButton = document.querySelector('.popup-button');
 const cancelButton = document.querySelector('.cancel-button');
 const addButton = document.querySelector('.add-button');
 const popup = document.querySelector('.popup');
-const ul = document.querySelector('ul');
-const li = document.querySelectorAll('.li-item');
+const list = document.querySelector('ul');
 const input = document.querySelector('input');
-const xmark = document.querySelectorAll('.fa-xmark');
-const list = document.querySelector('.list');
+let liEl = document.querySelectorAll('.li-item');
+let xmarkEl = document.querySelectorAll('.fa-xmark');
+// const listWrapper = document.querySelector('.list-wrapper');
 
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let todos = JSON.parse(localStorage.getItem('items')) || [];
+
+displayTodos();
+liEl = document.querySelectorAll('.li-item');
+xmarkEl = document.querySelectorAll('.fa-xmark');
 
 popupButton.addEventListener('click', () => {
     popup.classList.add('show');
     input.focus();
-    ul.style.opacity = '0.5';
+    list.style.opacity = '0.5';
     popupButton.classList.add('hide');
     addButton.classList.remove('hide');
     cancelButton.classList.remove('hide');
 
     if (!popup.classList.contains('show')) {
-        ul.style.opacity = '';
+        list.style.opacity = '';
     }
 });
 
 function saveData(){
     addButton.addEventListener('click', () => {
         if (input.value.trim() === '') {
-            firstStage();
+            alert('Todo is empty');
+            input.focus();
         } else {
             addTask();
+            displayTodos();
         }
     });
     
@@ -45,53 +51,68 @@ cancelButton.addEventListener('click', () => {
     input.value = '';
 });
 
-// ul.addEventListener('click', (e) => {
-//     if (e.target.contains('li')) {
-//         e.target.classList.toggle('complete');
-//     }
-// });
+    xmarkEl.forEach(xmark => {
+        xmark.addEventListener('click', e => {
+            for (let i = 0; i < liEl.length; i++) {
+                
+            }
+            
+        });
+    });
 
-ul.addEventListener('click', (e) => {
-    let target = e.target.closest('li');
-    if (target){
-        target.innerHTML = '';
-    }
+// delete todos from localStorage and save it (need to check last one)
+
+xmarkEl.forEach(elem => {
+    elem.addEventListener('click', (e) => {
+
+        todos.filter(() => {
+            //textContent
+            if(e.textContent) {
+                return e.textContent == '';
+            }
+        });
+
+        elem.parentElement.remove();
+
+    });
 });
 
 // functions
 
 function firstStage() {
     popup.classList.remove('show');
-    ul.style.opacity = '';
+    list.style.opacity = '';
     popupButton.classList.remove('hide');
     addButton.classList.add('hide');
     cancelButton.classList.add('hide');
 }
 
 function addTask() {
-        itemsArray.push(input.value);
-        localStorage.setItem('items', JSON.stringify(itemsArray));
+    todos.push(input.value);
+    // localStorage.setItem('items', JSON.stringify(todos));
+    
+    // if (input.value.t) {
+    //     alert();
+    // }
+
+    let li = document.createElement('li');
+    li.classList.add('li-item');
+    li.innerHTML += `<div class="square"></div><i class="fa-solid fa-xmark"></i>${input.value}`;
+    list.append(li);
+    input.value = '';
+    firstStage();
+
+    liEl = document.querySelectorAll('.li-item');
+    xmarkEl = document.querySelectorAll('.fa-xmark');
+}
+
+function displayTodos() {
+    for (let i = 0; i < todos.length; i++) {
     
         let li = document.createElement('li');
         li.classList.add('li-item');
-        li.innerHTML += `<div class="square"></div><i class="fa-solid fa-xmark"></i>${input.value}`;
-    
-        ul.append(li);
-        input.value = '';
-        firstStage();
-}
+        li.innerHTML += `<div class="square"></div><i class="fa-solid fa-xmark"></i>${todos[i]}`;
 
-// SMTH NEW
-
-function displayItems() {
-
-    for (let i = 0; i < itemsArray.length; i++) {
-    
-    let li = document.createElement('li');
-    li.classList.add('li-item');
-    li.innerHTML += `<div class="square"></div><i class="fa-solid fa-xmark"></i>${itemsArray[i]}`;
-
-    ul.append(li);
+        list.append(li);
     }
 }
-displayItems();
