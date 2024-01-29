1ee1030cd1d07d87705a824c7a4b6c826372321e
Artem Tyshkov <artemtyshkov@gmail.com>
2024-01-29 13:21:33 +0200

    Added li vertical alignment and formatting for files

--- script.js
+++ script.js
@@ -1,103 +1,113 @@
-const popupButton = document.querySelector('.popup-button');
-const cancelButton = document.querySelector('.cancel-button');
-const addButton = document.querySelector('.add-button');
-const popup = document.querySelector('.popup');
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
+const popupButton = document.querySelector(".popup-button");
+const cancelButton = document.querySelector(".cancel-button");
+const addButton = document.querySelector(".add-button");
+const popup = document.querySelector(".popup");
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
 
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 renderTodos();
 
-popupButton.addEventListener('click', () => {
-    popup.classList.add('show');
+popupButton.addEventListener("click", () => {
+    popup.classList.add("show");
     input.focus();
-    list.style.opacity = '0.5';
-    popupButton.classList.add('hide');
-    addButton.classList.remove('hide');
-    cancelButton.classList.remove('hide');
+    list.style.opacity = "0.5";
+    popupButton.classList.add("hide");
+    addButton.classList.remove("hide");
+    cancelButton.classList.remove("hide");
 
-    if (!popup.classList.contains('show')) {
-        list.style.opacity = '';
+    if (!popup.classList.contains("show")) {
+        list.style.opacity = "";
     }
 });
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener("click", () => {
     saveTodo();
     renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 });
 
-input.addEventListener('keyup', (e) => {
-    if (e.code === 'Enter' && input.value === '') {
-        alert('Todo is empty');
+input.addEventListener("keyup", (e) => {
+    if (e.code === "Enter" && input.value === "") {
+        alert("Todo is empty");
         input.focus();
-    } else if (e.code === 'Enter') {
+    } else if (e.code === "Enter") {
         saveTodo();
         renderTodos();
-        localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem("todos", JSON.stringify(todos));
     }
 });
 
-cancelButton.addEventListener('click', () => {
+cancelButton.addEventListener("click", () => {
     firstStage();
-    input.value = '';
+    input.value = "";
 });
 
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
     const target = e.target;
 
-    if (!target.classList.contains('li-item') && 
-        target.className !== 'square' && 
-        !target.classList.contains('fa-solid')) {return;}
+    if (
+        !target.classList.contains("li-item") &&
+        target.className !== "square" &&
+        !target.classList.contains("fa-solid")
+    ) {
+        return;
+    }
 
     const todoId = Number(target.parentElement.id) || Number(target.id);
 
     const action = target.dataset.action;
     console.log(action);
-    if (action === 'check') {
+    if (action === "check") {
         checkTodo(todoId);
     }
 
-    if (action === 'delete') {
+    if (action === "delete") {
         deleteTodo(todoId);
     }
 });
 
 function firstStage() {
-    popup.classList.remove('show');
-    list.style.opacity = '';
-    popupButton.classList.remove('hide');
-    addButton.classList.add('hide');
-    cancelButton.classList.add('hide');
+    popup.classList.remove("show");
+    list.style.opacity = "";
+    popupButton.classList.remove("hide");
+    addButton.classList.add("hide");
+    cancelButton.classList.add("hide");
 }
 
 function saveTodo() {
     const isDuplicate = todos.some((todo) => todo.value === input.value);
 
-    if (input.value === '') {
-        alert('Todo is empty');
+    if (input.value === "") {
+        alert("Todo is empty");
         input.focus();
     } else if (isDuplicate) {
-        alert('Todo already exists');
+        alert("Todo already exists");
     } else {
         const todo = {
             value: input.value,
-            checked: false
+            checked: false,
         };
-    
+
         todos.push(todo);
-        input.value = '';
+        input.value = "";
         firstStage();
     }
 }
 
 function renderTodos() {
-    list.innerHTML = '';
+    list.innerHTML = "";
     todos.forEach((todo, index) => {
         list.innerHTML += `
-            <li class="li-item ${todo.checked ? "completed" : ""}" data-action="check" id="${index}">
-                <div class="square" data-action="check">${todo.checked ? `<i class="fa-solid fa-check" style="color: #ffffff;"></i>` : ""}</div>
+            <li class="li-item ${
+                todo.checked ? "completed" : ""
+            }" data-action="check" id="${index}">
+                <div class="square" data-action="check">${
+                    todo.checked
+                        ? `<i class="fa-solid fa-check" style="color: #ffffff;"></i>`
+                        : ""
+                }</div>
                 <i class="fa-solid fa-xmark" data-action="delete"></i>
             ${todo.value}
             </li>
@@ -107,16 +117,16 @@ function renderTodos() {
 
 function checkTodo(todoId) {
     todos = todos.map((todo, index) => ({
-            ...todo,
-            checked: index === todoId ? !todo.checked : todo.checked,
+        ...todo,
+        checked: index === todoId ? !todo.checked : todo.checked,
     }));
 
     renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function deleteTodo(todoId) {
     todos = todos.filter((todo, index) => index !== todoId);
     renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
